abec0deae511fb97bc2a7a8f727285b6623269ae
Cecelia-1018 <jieshi1018@gmail.com>
2022-06-16 18:21:35 +0800

    upload new change

--- hackernews-react-apollo/src/components/LinkList.js
+++ hackernews-react-apollo/src/components/LinkList.js
@@ -1,28 +1,35 @@
 import React from 'react';
 import Link from './Link';
+import { useQuery, gql } from '@apollo/client';
 
-const LinkList = () => {
-  const linksToRender = [
-    {
-      id: 'link-id-1',
-      description:
-        'Prisma gives you a powerful database toolkit 😎',
-      url: 'https://prisma.io'
-    },
-    {
-      id: 'link-id-2',
-      description: 'The best GraphQL client',
-      url: 'https://www.apollographql.com/docs/react/'
+const FEED_QUERY = gql`
+  {
+    feed {
+      id
+      links {
+        id
+        createdAt
+        url
+        description
+      }
     }
-  ];
-
-  return (
-    <div>
-      {linksToRender.map((link) => (
-        <Link key={link.id} link={link} />
-      ))}
-    </div>
-  );
-};
+  }
+`
+;
+const LinkList = () => {
+    const { data } = useQuery(FEED_QUERY);
+  
+    return (
+      <div>
+        {data && (
+          <>
+            {data.feed.links.map((link) => (
+              <Link key={link.id} link={link} />
+            ))}
+          </>
+        )}
+      </div>
+    );
+  };
 
 export default LinkList;
