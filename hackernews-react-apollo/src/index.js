ccb033f2aa1af32f7a0a4b9d5a6e09e2abed482e
Cecelia-1018 <jieshi1018@gmail.com>
2022-06-17 17:08:10 +0800

    Realtime update

--- hackernews-react-apollo/src/index.js
+++ hackernews-react-apollo/src/index.js
@@ -13,10 +13,19 @@ import {
 } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
 import { AUTH_TOKEN } from './constants';
+import { split } from '@apollo/client';
+import { WebSocketLink } from '@apollo/client/link/ws';
+import { getMainDefinition } from '@apollo/client/utilities';
 
 // 2
-const httpLink = createHttpLink({
-  uri: 'http://localhost:4000'
+const wsLink = new WebSocketLink({
+  uri: `ws://localhost:4000/graphql`,
+  options: {
+    reconnect: true,
+    connectionParams: {
+      authToken: localStorage.getItem(AUTH_TOKEN)
+    }
+  }
 });
 
 const authLink = setContext((_, { headers }) => {
@@ -29,9 +38,29 @@ const authLink = setContext((_, { headers }) => {
   };
 });
 
+const httpLink = createHttpLink({
+  uri: 'http://localhost:4000'
+});
+
+
+const link = split(
+  ({ query }) => {
+    const { kind, operation } = getMainDefinition(query);
+    return (
+      kind === 'OperationDefinition' &&
+      operation === 'subscription'
+    );
+  },
+  wsLink,
+  authLink.concat(httpLink)
+);
+
+
+
+
 // 3
 const client = new ApolloClient({
-  link: authLink.concat(httpLink),
+  link,
   cache: new InMemoryCache()
 });
 
