5c2e2f78f17d01f65bcbed9b2da89694a5404725
Cecelia-1018 <jieshi1018@gmail.com>
2022-06-20 09:14:58 +0800

    add header

--- hackernews-react-apollo/src/components/Header.js
+++ hackernews-react-apollo/src/components/Header.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import { Link, useNavigate } from 'react-router-dom';
-import { AUTH_TOKEN } from '../constants';
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { AUTH_TOKEN } from "../constants";
 
 const Header = () => {
   const navigate = useNavigate();
@@ -10,24 +10,22 @@ const Header = () => {
       <div className="flex flex-fixed black">
         <Link to="/" className="no-underline black">
           <div className="fw7 mr1">Hacker News</div>
-        </Link>           
+        </Link>
         <Link to="/" className="ml1 no-underline black">
           new
         </Link>
         <div className="ml1">|</div>
-        <Link
-          to="/search"
-          className="ml1 no-underline black"
-        >
+        <Link to="/top" className="ml1 no-underline black">
+          top
+        </Link>
+        <div className="ml1">|</div>
+        <Link to="/search" className="ml1 no-underline black">
           search
         </Link>
         {authToken && (
           <div className="flex">
             <div className="ml1">|</div>
-            <Link
-              to="/create"
-              className="ml1 no-underline black"
-            >
+            <Link to="/create" className="ml1 no-underline black">
               submit
             </Link>
           </div>
@@ -45,10 +43,7 @@ const Header = () => {
             logout
           </div>
         ) : (
-          <Link
-            to="/login"
-            className="ml1 no-underline black"
-          >
+          <Link to="/login" className="ml1 no-underline black">
             login
           </Link>
         )}
