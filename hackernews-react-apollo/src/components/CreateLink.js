e763ad996752483e2807025a90a4ca092572191f
Cecelia-1018 <jieshi1018@gmail.com>
2022-06-20 09:39:15 +0800

    final

--- hackernews-react-apollo/src/components/CreateLink.js
+++ hackernews-react-apollo/src/components/CreateLink.js
@@ -1,12 +1,11 @@
-import React, { useState } from 'react';
-import { useMutation, gql } from '@apollo/client';
-import { useNavigate } from 'react-router-dom';
+import React, { useState } from "react";
+import { useMutation, gql } from "@apollo/client";
+import { useNavigate } from "react-router-dom";
+import { AUTH_TOKEN, LINKS_PER_PAGE } from "../constants";
+import {FEED_QUERY} from "./LinkList";
 
 const CREATE_LINK_MUTATION = gql`
-  mutation PostMutation(
-    $description: String!
-    $url: String!
-  ) {
+  mutation PostMutation($description: String!, $url: String!) {
     post(description: $description, url: $url) {
       id
       createdAt
@@ -18,26 +17,57 @@ const CREATE_LINK_MUTATION = gql`
 const CreateLink = () => {
   const navigate = useNavigate();
   const [formState, setFormState] = useState({
-    description: '',
-    url: ''
+    description: "",
+    url: "",
   });
 
+  // ...
   const [createLink] = useMutation(CREATE_LINK_MUTATION, {
     variables: {
       description: formState.description,
-      url: formState.url
+      url: formState.url,
     },
-    onCompleted: () => navigate('/')
+    update: (cache, { data: { post } }) => {
+      const take = LINKS_PER_PAGE;
+      const skip = 0;
+      const orderBy = { createdAt: "desc" };
+
+      const data = cache.readQuery({
+        query: FEED_QUERY,
+        variables: {
+          take,
+          skip,
+          orderBy,
+        },
+      });
+
+      cache.writeQuery({
+        query: FEED_QUERY,
+        data: {
+          feed: {
+            links: [post, ...data.feed.links],
+          },
+        },
+        variables: {
+          take,
+          skip,
+          orderBy,
+        },
+      });
+    },
+    onCompleted: () => {
+      navigate("/");
+    },
   });
 
   return (
     <div>
-     <form
-      onSubmit={(e) => {
-        e.preventDefault();
-        createLink();
-      }}
-    >
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          createLink();
+        }}
+      >
         <div className="flex flex-column mt3">
           <input
             className="mb2"
@@ -45,7 +75,7 @@ const CreateLink = () => {
             onChange={(e) =>
               setFormState({
                 ...formState,
-                description: e.target.value
+                description: e.target.value,
               })
             }
             type="text"
@@ -57,7 +87,7 @@ const CreateLink = () => {
             onChange={(e) =>
               setFormState({
                 ...formState,
-                url: e.target.value
+                url: e.target.value,
               })
             }
             type="text"
